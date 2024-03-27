cb16add899720561b9359ea099859aed327c9848
EvrettG <skyrift@tpg.com.au>
2024-03-27 20:37:37 +1100

    Blog data now renders on blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,37 +1,53 @@
-// adds blogposts to blog page
-const blogSection = document.querySelector('#blogSection');
 
+// adds blogposts to blog page
 function renderBlogs() {
 
-    blogSection.innerHTML = '';
+    // Unnessecary code? Test more later
+    // const blogSection = document.querySelector('#blogSection');
+    // blogSection.innerHTML = '';
 
-    for(let x =0; x ,blogs.length; x++) {
+    // creates a new div and poulates it with data from name text and title from blogPosts local storage
+
+    // add error mesage or something later for no blogPosts data
+    for(let x =0; x < blogs.length; x++) {
         const blog = blogs[x];
+        // sets the location for the div to be appended to
+        let target = document.getElementById("blogSection")
 
+        // creates the new elements to be appended to the target
         const divEl = document.createElement('div');
-
         const head3 = document.createElement('h3');
         const para = document.createElement('p');
         const head4 = document.createElement('h4');
+        
+        // console log for testing function running
+        console.log("test1.1")
 
-        divEl.Element.setAttribute('class', 'posted')
+        // sets class to div for css styling
+        divEl.setAttribute("class", "posted");
 
-        head3.textContent = 
-        para.textContent = 
-        head4.textContent =
+        // adds data related to each part of the blogPosts
+        head3.textContent = blog.name
+        para.textContent =  blog.text
+        head4.textContent = blog.title
 
+        // appendes the new div element to the target section
+        target.appendChild(divEl);
+
+        // appends the visable content to the div
         divEl.appendChild(head3);
         divEl.appendChild(para);
         divEl.appendChild(head4);
         
+        // console log for testing function running
         console.log("test 2");
+        // console log to advise where in for loop function is running
         console.log(x);
     }
 
 }
 
 // 
-const myDiv = document.getElementById('myDiv');
 
 
 
